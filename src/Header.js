38ecc36245de2d53640aa82bebb794f6e7396ce2
Xenz <ravensvenix@gmail.com>
2023-07-30 21:17:01 +0800

    Portfolio

---
+++ src/Header.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { socialMediaUrl } from "../Details";
+import logo from '../assets/logo.jpg';
+const instagramSvg = '<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"/></svg>';
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { github, instagram } = socialMediaUrl;
+  const toggleClass = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="container mx-auto md:flex justify-between py-2 max-width">
+      <div className="flex justify-between items-center py-2 md:py-10">
+        <NavLink to="/">
+          <img className="w-14" src={logo} alt="logo" />
+        </NavLink>
+        <div onClick={toggleClass} className="cursor-pointer">
+          <svg
+            className="stroke-dark-heading dark:stroke-white md:hidden"
+            width="25"
+            height="20"
+            viewBox="0 0 16 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.4375 1.3125H14.5625M1.4375 11.3125H14.5625H1.4375ZM1.4375 6.3125H14.5625H1.4375Z"
+              strokeWidth="1.875"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+      <nav className={` ${!isOpen ? "hidden" : null} text-center md:flex justify-between`}>
+        <ul className="dark:text-light-content font-medium md:flex items-center md:space-x-5 md:mr-10">
+          <li className="pb-1 md:pb-0">
+            <NavLink to="/" onClick={toggleClass}>
+              Home
+            </NavLink>
+          </li>
+          <li className="pb-1 md:pb-0">
+            <NavLink to="/about" onClick={toggleClass}>
+              About
+            </NavLink>
+          </li>
+          <li className="pb-1 md:pb-0">
+            <NavLink to="/technologies" onClick={toggleClass}>
+              Technologies
+            </NavLink>
+          </li>
+          <li className="pb-1 md:pb-0">
+            <NavLink to="/projects" onClick={toggleClass}>
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" onClick={toggleClass}>
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <ul className="flex justify-evenly items-center my-5 md:my-0 md:space-x-5 md:mr-5">
+          <li>
+            <a href={instagram} target="_blank" rel="noreferrer noopener">
+              <div dangerouslySetInnerHTML={{ __html: instagramSvg }} />
+            </a>
+          </li>
+		  <li>
+            <a href={github} target="_blank" rel="noreferrer noopener">
+              <svg
+                className="dark:fill-light-heading fill-dark-heading"
+                width="30"
+                height="30"
+                vviewBox="0 0 30 30"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M15 0C6.7125 0 0 6.7125 0 15C0 21.6375 4.29375 27.2437 10.2563 29.2313C11.0063 29.3625 11.2875 28.9125 11.2875 28.5188C11.2875 28.1625 11.2688 26.9813 11.2688 25.725C7.5 26.4188 6.525 24.8062 6.225 23.9625C6.05625 23.5312 5.325 22.2 4.6875 21.8438C4.1625 21.5625 3.4125 20.8687 4.66875 20.85C5.85 20.8313 6.69375 21.9375 6.975 22.3875C8.325 24.6562 10.4812 24.0187 11.3438 23.625C11.475 22.65 11.8688 21.9937 12.3 21.6187C8.9625 21.2437 5.475 19.95 5.475 14.2125C5.475 12.5813 6.05625 11.2313 7.0125 10.1813C6.8625 9.80625 6.3375 8.26875 7.1625 6.20625C7.1625 6.20625 8.41875 5.8125 11.2875 7.74375C12.4875 7.40625 13.7625 7.2375 15.0375 7.2375C16.3125 7.2375 17.5875 7.40625 18.7875 7.74375C21.6562 5.79375 22.9125 6.20625 22.9125 6.20625C23.7375 8.26875 23.2125 9.80625 23.0625 10.1813C24.0188 11.2313 24.6 12.5625 24.6 14.2125C24.6 19.9688 21.0938 21.2437 17.7563 21.6187C18.3 22.0875 18.7688 22.9875 18.7688 24.3937C18.7688 26.4 18.75 28.0125 18.75 28.5188C18.75 28.9125 19.0312 29.3813 19.7812 29.2313C22.759 28.2259 25.3465 26.3121 27.1796 23.7592C29.0127 21.2063 29.9991 18.1429 30 15C30 6.7125 23.2875 0 15 0Z"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
