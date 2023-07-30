7399436a15795e27081b6830bdc9135158de3e39
Xenz <ravensvenix@gmail.com>
2023-07-30 20:44:59 +0800

    Portfolio

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -11,7 +11,7 @@ function Projects() {
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10">
           {React.Children.toArray(
-            projectsToShow.map(
+            projectDetails.map(
               ({ title, image, description, techstack, previewLink, githubLink }) => (
                 <Project
                   title={title}
