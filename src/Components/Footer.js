4546582a9a3f8fb943dab07f52e7bf3be0cd267c
Xenz <ravensvenix@gmail.com>
2023-07-30 20:34:41 +0800

    Portfolio

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,18 +3,17 @@ function Footer() {
   return (
     <footer className="container mx-auto py-1 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
       <p className="text-xs text-center text-dark-content dark:text-light-content w-full">
-        Designed and Coded by{" "}
+        Designed by{" "}
         <a
           className="font-medium"
-          href="https://pavanmg.in"
+          href="https://xenzz.com/"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Pavan MG
+          Xenz
         </a>{" "}
         with
-        <span className="text-gradient font-medium"> Love</span> &
-        <span className="text-gradient font-medium"> Coffee</span>
+        <span className="text-gradient font-medium"> Love</span>
       </p>
     </footer>
   );
